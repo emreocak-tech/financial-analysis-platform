b8ca1e99d4c5ce40c39fcf9a32a152e2d1b27a8c
Emre Ocak <ocakemre2005@gmail.com>
2026-02-24 21:54:47 +0300

    crude_oil.py and silver.py are updated.

---
+++ crude_oil.py
@@ -0,0 +1,73 @@
+from dotenv import load_dotenv
+load_dotenv()
+from prophet import Prophet
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+crude_oil_path=os.getenv("CRUDE_OIL")
+df=pd.read_csv(crude_oil_path)
+from abc import ABC,abstractmethod
+class AbstractOil(ABC):
+    def __init__(self):
+        super().__init__()
+    def show_information(self):
+        pass
+    def show_graph(self):
+        pass
+    def machine_learning_model(self):
+        pass
+class CrudeOil(AbstractOil):
+    def show_information(self):
+        try:
+            print(f"The average closing price over the last 30 days {df.tail(30)['price'].mean()} dollars! ")
+            print(f"The average maximum price over the last 30 days : {df.tail(30)['price'].max()} dollars!")
+            print(f"The average minimum price over the last 30 days : {df.tail(30)['price'].min()} dollars!")
+        except ValueError as v_error:
+            print(f"There was an value error : Error Code : {v_error}!")
+        except FileNotFoundError as file_error:
+            print(f"Fine was not found try again , Error Code :  {file_error}!")
+        except Exception as except_error:
+            print(f"There was an generally error try again , Error Code : {except_error}!")
+    def show_graph(self):
+        try:
+            plt.plot(df.tail(30)['date'],df.tail(30)['price'],color="Black",linewidth=3,label="Crude Oil Price")
+            plt.xlabel("Date",color="Black",fontsize=18)
+            plt.ylabel("Price",color="Black",fontsize=18)
+            plt.title("CRUDE OIL PRICE",color="Black",fontsize=20)
+            plt.legend()
+            plt.grid()
+            plt.show()
+        except ValueError as v_error:
+            print(f"There was an value error : Error Code : {v_error}!")
+        except FileNotFoundError as file_error:
+            print(f"Fine was not found try again , Error Code :  {file_error}!")
+        except Exception as except_error:
+            print(f"There was an generally error try again , Error Code : {except_error}!")
+    def machine_learning_model(self):
+        try:
+            af = df[['date', 'price']].copy()
+            af.columns = ['ds', 'y']
+            af = af.dropna()
+            af = af[af['y'] > 0]
+            af['ds'] = pd.to_datetime(af['ds'])  # 1. ÖNCE çevir
+            if af['ds'].dt.tz is not None:  # 2. SONRA kontrol et
+                af['ds'] = af['ds'].dt.tz_convert(None)
+            af['y'] = np.log(af['y'])
+            model = Prophet(changepoint_prior_scale=0.1, yearly_seasonality=True)
+            model.fit(af)
+            future = model.make_future_dataframe(periods=15)
+            predict = model.predict(future)
+            tahmin_degeri = predict.iloc[-1]['yhat']
+            tahmin_degeri = np.exp(tahmin_degeri)
+            return [tahmin_degeri, predict, model]
+        except ValueError as v_error:
+            print(f"There was an value error : Error Code : {v_error}!")
+        except FileNotFoundError as file_error:
+            print(f"Fine was not found try again , Error Code :  {file_error}!")
+        except Exception as except_error:
+            print(f"There was an generally error try again , Error Code : {except_error}!")
+asd=CrudeOil()
+qwe=asd.machine_learning_model()
+print(qwe[0])
+
--- silver.py
+++ silver.py
@@ -4,21 +4,22 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader,TensorDataset
-import requests
 import os
-import datetime
+from torch.utils.data import DataLoader,TensorDataset
 from dotenv import load_dotenv
 load_dotenv()
 from abc import ABC,abstractmethod
 silver_path=os.getenv("SILVER_PATH")
 df=pd.read_csv(silver_path)
+oil_path=os.getenv("CRUDE_OIL")
+oil_df=pd.read_csv(oil_path)
 """
 specific_date = '2026-01-13'
 filtered_data = df[df['Date'] == specific_date]['Close']
 print(filtered_data)
 """
-class AbstractGold(ABC):
+
+class AbstractSilver(ABC):
     def __init__(self):
         super().__init__()
     @abstractmethod
@@ -27,9 +28,10 @@ class AbstractGold(ABC):
     @abstractmethod
     def show_graph(self):
         pass
+    @abstractmethod
     def model_silver(self):
         pass
-class Silver(AbstractGold):
+class Silver(AbstractSilver):
     def price_silver(self):
         try:
             print(f"The average closing price over the last 30 days {df.tail(30)['Close'].mean()} dollars! ")
@@ -94,9 +96,106 @@ class Silver(AbstractGold):
             print(f"Fine was not found try again , Error Code :  {file_error}!")
         except Exception as except_error:
             print(f"There was an generally error try again , Error Code : {except_error}!")
+    def compare_oil(self):
+        try:
+            plt.plot(df.tail(30)['Date'], df.tail(30)['Close'], color="Silver", linewidth=3, label="Silver Price")
+            plt.plot(oil_df.tail(30)['date'], oil_df.tail(30)['price'], color="Black", linewidth=3,label="Crude Oil Price")
+            plt.xlabel("Date", color="Black", fontsize=18)
+            plt.ylabel("Price", color="Black", fontsize=18)
+            plt.title("SILVER-CRUDE OIL", color="Black", fontsize=20)
+            plt.legend()
+            plt.grid()
+            plt.show()
+        except ValueError as v_error:
+            print(f"There was an value error : Error Code : {v_error}!")
+        except FileNotFoundError as file_error:
+            print(f"Fine was not found try again , Error Code :  {file_error}!")
+        except Exception as except_error:
+            print(f"There was an generally error try again , Error Code : {except_error}!")
+    def model_silver(self):
+        try:
+            values=df['Close'].values
+            def create_sequences(values,seq_length=3):
+                x=[]
+                y=[]
+                for i in range(len(values)-seq_length):
+                    prices=values[i:i+seq_length]
+                    target=values[i+3]
+                    x.append(prices)
+                    y.append(target)
+                return np.array(x),np.array(y)
 
-qwe=Silver()
-qwe.calculate_rsi()
+            x, y = create_sequences(values, seq_length=3)
+            total = len(x)
+            train_size = int(0.6 * total)  # %60 train
+            val_size = int(0.2 * total)  # %20 validation
+            test_size = total - train_size - val_size  # %20 test
+
+            x_train = x[:train_size]
+            y_train = y[:train_size]
+
+            x_val = x[train_size:train_size + val_size]
+            y_val = y[train_size:train_size + val_size]
+
+            x_test = x[train_size + val_size:]
+            y_test = y[train_size + val_size:]
+
+            x_train_tensor = torch.FloatTensor(x_train).unsqueeze(-1)  # (N, 3, 1)
+            y_train_tensor = torch.FloatTensor(y_train).unsqueeze(-1)  # (N, 1)
+
+            def normalizition(tensor,maxs,mins):
+                return (tensor-mins)/(maxs-mins)
+            maxs_one=x_train_tensor.max()
+            mins_one=x_train_tensor.min()
+            maxs_two=y_train_tensor.max()
+            mins_two=y_train_tensor.min()
+
+            x_train_tensor=normalizition(x_train_tensor,maxs_one,mins_one)
+            y_train_tensor=normalizition(y_train_tensor,maxs_two,mins_two)
+
+            x_val_tensor = torch.FloatTensor(x_val).unsqueeze(-1)  # (N, 3, 1)
+            y_val_tensor = torch.FloatTensor(y_val).unsqueeze(-1)
+
+            x_test_tensor = torch.FloatTensor(x_test).unsqueeze(-1)
+            y_test_tensor = torch.FloatTensor(y_test).unsqueeze(-1)
+            class Lstm_Model(nn.Module):
+                def __init__(self):
+                    super().__init__()
+                    self.lstm = nn.LSTM(1, 10, batch_first=True)
+                    self.shell1 = nn.Linear(10, 1)
+                def forward(self, x):
+                    x = self.lstm(x)
+                    x = x[:, -1, :]
+                    x = self.shell1(x)
+                    return x
+            model = Lstm_Model()
+            optimizer = optim.Adam(model.parameters(), lr=0.01)
+            criterion = nn.MSELoss()
+            for epoch in range(1000):
+                prediction = model(x_train_tensor)
+                loss = criterion(prediction, y_train_tensor)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                if epoch % 100 == 0:
+                    print(f"The loss value is {loss.item()}")
+            model.eval()
+            with torch.no_grad():
+                prediction = model(x_test_tensor)
+                loss = criterion(prediction, y_test_tensor)
+                print(f"The test loss value is {loss.item()}")
+                print("The model was educated successfully !")
+        except ValueError as v_error:
+            print(f"There was an value error : Error Code : {v_error}!")
+        except FileNotFoundError as file_error:
+            print(f"Fine was not found try again , Error Code :  {file_error}!")
+        except Exception as except_error:
+            print(f"There was an generally error try again , Error Code : {except_error}!")
+
+
+
+
+
 
 
 
